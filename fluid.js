5b419d94c7649de781dc6399afab587ae6011b01
kakaixi <kakaxi@gmail.com>
2023-02-09 22:23:23 +0800

    [add]

--- fluid.js
+++ fluid.js
@@ -8,21 +8,24 @@ fluid.prototype = {
         var oHead = document.getElementsByTagName('HEAD').item(0);
         var oScript= document.createElement("script");
         oScript.type = "text/javascript";
-        oScript.src="jquery.js";
+        oScript.src="https://payment-test.gofluid.io/web/jquery.js";
         oHead.appendChild( oScript);
         console.log("init fluid");
     },
-    openDialog: function () {
+    openDialog: function (url) {
         var $ = window.jQuery
         var dialogDom = $(
             '<div class="fluid-xb-bg">'+
                 '<div class="fluid-xb-box">'+
                     '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAlCAYAAAAqXEs9AAAACXBIWXMAACE4AAAhOAFFljFgAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAADYSURBVHgB1ZXrCcMwEINFJ9H+S3iUjpImkEIKbey7k1LnwPiH9fjAeQCfw3W1fXcPe13bwXNdy74TXpjTrqNggReKva5vAhcUR7raD4Eaiicw79VGhVUoRjvCBidM2eiAkQU4shRBigxZYMUrh8p4bFARbWk4WHQJTATqMpgqlAUmC2WFiUKlYB64+RATXRkx0UNNTPTaExN9GImJfh1MFGQ8NhiFVw6jzNAGKbLKAcrMtNEBFTa4oRq8MBGo1hOqYEagutemhgl3HYUumHDXdtDghflHV35e5EwCm+oUFI0AAAAASUVORK5CYII=" class="fluid-xb-close">'+
-                        '<iframe src="http://localhost:8082/#/home?order_number=627648719168212992&company_id=111" class="fluid-xb-iframe-box"></iframe>'+
+                        '<iframe src="' + url + '" class="fluid-xb-iframe-box"></iframe>'+
                 '</div>'+
             '</div>'
         )
         $("body").append(dialogDom)
+        $(".fluid-xb-close").click(function (){
+            $(".fluid-xb-bg").hide()
+        })
         console.log($)
     }
 }
